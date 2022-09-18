0cff8dc49bc8fe9f04a1917e2a5dee2c9ee2a1d6
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-09-18 23:57:40 +0500

    update results

--- api/views.py
+++ api/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from api.models import *
-
+from django.db.models import Q
 def home(request):
     return render(request, "pages/home.html")
 
 def search_view(request):
     get = request.GET.get("search", "")
-    if get and get != "":
-        api = Api.objects.filter(title__contains=get).all()
+    if 'search' in request.GET:
+        search = request.GET['search']
+        full_search = Q(Q(artist__icontains=search) | Q(Q(title__icontains=search)))
+        api = Api.objects.filter(full_search)[:1]
     else:
-        api = ''
+        api = Api.objects.all()
     context = {
         "api":api,
         "get":get
     }
-    return render(request, "pages/search.html", context)
+    return render(request, "pages/results.html", context)
